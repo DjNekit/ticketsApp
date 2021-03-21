d72cd9578cb7b17d42e3d12afa6cc8339889eb06
nikita <nikitalipin97@gmail.com>
2021-03-21 20:02:29 +0300

    Add abstract class for all custom errors

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,15 +1,17 @@
-export class DataBaseConnectionError extends Error {
+import { CustomError } from './custom-error'
+
+export class DataBaseConnectionError extends CustomError {
     statusCode = 500
     reason = 'Error connecting to database'
 
     constructor() {
-        super()
+        super('Error connecting to database')
         Object.setPrototypeOf(this, DataBaseConnectionError.prototype)
     }
 
     serializeErrors() {
-        return [{
-            message: this.reason
-        }]
+        return {
+            errors: [{ message: this.reason }]
+        }
     }
 }
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,15 +1,11 @@
 import { Request, Response, NextFunction } from 'express'
-import { RequestValidationError } from '../errors/request-validation-error'
-import { DataBaseConnectionError } from '../errors/database-connection-error'
+import { CustomError } from '../errors/custom-error'
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err instanceof RequestValidationError) {
+    if (err instanceof CustomError) {
         return res.status(err.statusCode).json(err.serializeErrors())
     }
-    if (err instanceof DataBaseConnectionError) {
-        return res.status(err.statusCode).json(err.serializeErrors())
-    }
-
+    
     res.status(400).json({
         errors: [{
             message: err.message
