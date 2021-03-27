544995744419f44f00069a12ff955b391bef64ae
nikita <nikitalipin97@gmail.com>
2021-03-27 18:02:52 +0300

    write tests for routes

---
+++ auth/src/app.ts
@@ -0,0 +1,40 @@
+import express from 'express'
+import helmet from 'helmet'
+import cookieSession from 'cookie-session'
+import 'express-async-errors'
+
+import { errorHandler } from './middlewares/error-handler'
+
+import { currentUserRouter } from './routes/current-user'
+import { signinRouter } from './routes/signin'
+import { signupRouter } from './routes/signup'
+import { signoutRouter } from './routes/signout'
+
+import { NotFoundError } from './errors/not-found-error'
+
+const app = express()
+
+app.set('trust proxy', true)
+
+// == Middlewares ============
+app.use(helmet())
+app.use(express.json())
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}))
+
+// == Routes =================
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signupRouter)
+app.use(signoutRouter)
+
+app.all('/*', async () => {
+    throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
+
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,44 +1,9 @@
-import express from 'express'
+import { app } from './app'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-import 'express-async-errors'
-
-
-// == Middlewares ============
-import { errorHandler } from './middlewares/error-handler'
-
-// == Routes =================
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signupRouter } from './routes/signup'
-import { signoutRouter } from './routes/signout'
-
-import { NotFoundError } from './errors/not-found-error'
-
-const PORT = 3000
-
-const app = express()
-
-app.set('trust proxy', true)
-
-app.use(express.json())
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
-
-app.all('/*', async () => {
-    throw new NotFoundError()
-})
-
-app.use(errorHandler)
 
 const start = async () => {
+    const PORT = 3000
+
     if (!process.env.JWT) {
         throw new Error('JWT env variable must be defined')
     }
