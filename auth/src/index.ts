4a2092ff3d2d33b512e7f07c795b7859f1306fb8
nikita <nikitalipin97@gmail.com>
2021-05-09 17:18:00 +0300

    add env MONGO_URI in auth and tickets

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,12 @@ const start = async () => {
     if (!process.env.JWT) {
         throw new Error('JWT env variable must be defined')
     }
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI env variable must be defined')
+    }
 
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
