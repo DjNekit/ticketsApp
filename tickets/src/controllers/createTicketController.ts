00e944cf484a19db4a58c6e5324256bc01b44136
nikita <nikitalipin97@gmail.com>
2021-05-22 16:24:58 +0300

    finish createTicketController

--- tickets/src/controllers/createTicketController.ts
+++ tickets/src/controllers/createTicketController.ts
@@ -2,9 +2,13 @@ import { Request, Response } from 'express'
 import { Ticket } from '../models/Ticket'
 
 export const createTicketController = async (req: Request, res: Response) => {
-  const { title, price, userId } = req.body
+  const { title, price } = req.body
 
-  const ticket = Ticket.build({ title, price, userId })
+  const ticket = Ticket.build({ 
+    title, 
+    price, 
+    userId: req.currentUser!.id 
+  })
   await ticket.save()
 
   res.status(201).json({ 
